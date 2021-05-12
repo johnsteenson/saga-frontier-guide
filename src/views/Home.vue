3873a95f2edea799e27ad61accbc30cc91d38055
John Steenson <johnmsteenson@gmail.com>
2021-05-11 23:16:56 -0700

    Added text to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <p>
+      SaGa Frontier is a complex game. There is a vast web of rules which
+      determine learning skills. This app is an interactive guide to simplify
+      that process to help you navigate the game. You can quickly look up a
+      skill and see which skills sparks it, as well as who is most likely to
+      learn it. Also, you can explore creating combos. This is still a work in
+      progress so be on the look out for new features.
+    </p>
+    <p>
+      The data used in this guide was gathered from this
+      <a
+        href="https://gamefaqs.gamespot.com/ps/198537-saga-frontier/faqs/58412"
+        target="_blank"
+        >excellent guide</a
+      >
+      by Zaraktheus on the mechanics of SaGa Frontier.
+    </p>
+    <p>
+      For a guide to the game as a whole, I highly recommend
+      <a href="http://snesmusic.jukor.net/junk/sagafaq.txt" target="_blank"
+        >Matt Hobbs' Walkthrough</a
+      >.
+    </p>
+    <p>
+      This project is open-source, so feel free to contribute or fork:
+      <a href="https://github.com/johnsteenson/saga-frontier-guide"
+        >https://github.com/johnsteenson/saga-frontier-guide</a
+      >
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
